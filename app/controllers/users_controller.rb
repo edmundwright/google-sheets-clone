02e0b7b0a10825850328732abd67c8c8c7e91af9
Edmund Wright <edmund.wright@gmail.com>
2015-09-14 21:32:42 -0400

    Create UsersController.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    filled_out_fields = session[:filled_out_fields] || {email: "", name: ""}
+    @user = User.new(filled_out_fields)
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to :root
+    else
+      session[:filled_out_fields] = user_params.delete_if do |key, _|
+        key == :password
+      end
+      redirect_to new_user_url
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password)
+  end
+end
