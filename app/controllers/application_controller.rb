6c7a1fd54f156e9793493fe987178afb729c4a1a
Edmund Wright <edmund.wright@gmail.com>
2015-09-14 22:13:12 -0400

    Add session helper methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def log_in_user!(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+  end
+
+  def log_out!
+    user.reset_session_token!
+    session[:session_token] = nil
+  end
 end
