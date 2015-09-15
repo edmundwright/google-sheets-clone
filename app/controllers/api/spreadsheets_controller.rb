ebcd39130d68a79c1a1c2423b00798ba410df9b7
Edmund Wright <edmund.wright@gmail.com>
2015-09-15 09:45:52 -0400

    Create SpreadsheetsController.

---
+++ app/controllers/api/spreadsheets_controller.rb
@@ -0,0 +1,41 @@
+class API::SpreadsheetsController < ApplicationController
+  def create
+    @spreadsheet = current_user.spreadsheets.new(spreadsheet_params)
+
+    if spreadsheet.save
+      render :show
+    else
+      render json: @spreadsheet.errors.full_messages
+    end
+  end
+
+  def index
+    @spreadsheets = current_user.spreadsheets
+  end
+
+  def show
+    @spreadsheet = current_user.spreadsheets.find(params[:id])
+  end
+
+  def update
+    @spreadsheet = current_user.spreadsheets.find(params[:id])
+
+    if spreadsheet.update(spreadsheet_params)
+      render :show
+    else
+      render json: @spreadsheet.errors.full_messages
+    end
+  end
+
+  def destroy
+    @spreadsheet = current_user.spreadsheets.find(params[:id])
+    @spreadsheet.destroy!
+    render :show
+  end
+
+  private
+
+  def spreadsheet_params
+    params.require(:spreadsheet).require(:title)
+  end
+end
