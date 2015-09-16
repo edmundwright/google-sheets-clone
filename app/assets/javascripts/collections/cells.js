39286c11f6905d7a3ec7d79951b987950a8b4fe2
Edmund Wright <edmund.wright@gmail.com>
2015-09-16 11:57:22 -0400

    Add comparator function to cells collection

--- app/assets/javascripts/collections/cells.js
+++ app/assets/javascripts/collections/cells.js
@@ -1,3 +1,15 @@
 GoogleSheetsClone.Collections.Cells = Backbone.Collection.extend({
-  model: GoogleSheetsClone.Models.Cell
+  model: GoogleSheetsClone.Models.Cell,
+
+  comparator: function (firstCell, secondCell) {
+    if (firstCell.get("row_index") < secondCell.get("row_index")) {
+      return -1;
+    } else if (firstCell.get("row_index") > secondCell.get("row_index")) {
+      return 1;
+    } else if (firstCell.get("col_index") < secondCell.get("col_index")) {
+      return -1;
+    } else {
+      return 1;
+    }
+  }
 });
