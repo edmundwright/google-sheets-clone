149a2b0f09493beb545fabeb788087e00deefc60
Edmund Wright <edmund.wright@gmail.com>
2015-09-17 14:52:18 -0400

    Make evaluator obey order of operations (I think).

--- app/assets/javascripts/utils/evaluate.js
+++ app/assets/javascripts/utils/evaluate.js
@@ -1,6 +1,10 @@
 (function () {
   var evaluate = GoogleSheetsClone.evaluate = function (formula) {
-    var newFormula = formula;
+    if (typeof formula === "number") {
+      return formula;
+    }
+
+    var newFormula = formula.replace(/ /g, '');
     var oldFormula = "";
 
     while (oldFormula != newFormula && isNaN(newFormula)) {
@@ -18,31 +22,38 @@
   var evaluateOnePart = function (formula) {
     var newFormula = "";
 
-    for(var i = 0; i < formula.length; i++) {
-      switch (formula[i]) {
-        case " ":
-          break;
-        case "(":
-          return evaluateBrackets(formula, i)
-        case "+":
-          return evaluateSimpleOperation(formula, i, "+");
-        case "-":
-          if (i !== 0) {
-            return evaluateSimpleOperation(formula, i, "-");
-          } else {
-            newFormula += formula[i];
-            break;
-          }
-        case "*":
-          return evaluateSimpleOperation(formula, i, "*");
-        case "/":
-          return evaluateSimpleOperation(formula, i, "/");
-        default:
-          newFormula += formula[i];
+    for(var i = formula.length - 1; i >= 0; i--) {
+      if (formula[i] === ")") {
+        return evaluateBrackets(formula, i)
+      }
+    }
+    for(var i = formula.length - 1; i >= 0; i--) {
+      if (formula[i] === "+") {
+        return evaluateSimpleOperation(formula, i, "+");
+      } else if (formula[i] === "-" &&
+                 i !== 0 &&
+                 formula[i-1] != "+" &&
+                 formula[i-1] != "-" &&
+                 formula[i-1] != "*" &&
+                 formula[i-1] != "/" &&
+                 formula[i-1] != "^") {
+        return evaluateSimpleOperation(formula, i, "-");
+      }
+    }
+    for(var i = formula.length - 1; i >= 0; i--) {
+      if (formula[i] === "*") {
+        return evaluateSimpleOperation(formula, i, "*");
+      } else if (formula[i] === "/") {
+        return evaluateSimpleOperation(formula, i, "/");
+      }
+    }
+    for(var i = formula.length - 1; i >= 0; i--) {
+      if (formula[i] === "^") {
+        return evaluateSimpleOperation(formula, i, "^");
       }
     }
 
-    return newFormula
+    return formula;
   };
 
   var evaluateSimpleOperation = function (formula, operatorPos, operator) {
@@ -58,22 +69,24 @@
           return leftHandSide * rightHandSide;
         case "/":
           return leftHandSide / rightHandSide;
+        case "^":
+          return Math.pow(leftHandSide, rightHandSide);
       }
     } else {
       return "INVALID-FORMULA!";
     }
   };
 
-  var evaluateBrackets = function (formula, openingBracketPos) {
-    for(var i = openingBracketPos + 1; i < formula.length; i++) {
+  var evaluateBrackets = function (formula, closingBracketPos) {
+    for(var i = closingBracketPos - 1; i >= 0; i--) {
       switch (formula[i]) {
-        case "(":
-          openingBracketPos = i;
-          break;
         case ")":
-          return formula.slice(0, openingBracketPos) +
-            evaluate(formula.slice(openingBracketPos + 1, i)) +
-            formula.slice(i + 1);
+          closingBracketPos = i;
+          break;
+        case "(":
+          return formula.slice(0, i) +
+            evaluate(formula.slice(i + 1, closingBracketPos)) +
+            formula.slice(closingBracketPos + 1);
       }
     }
 
