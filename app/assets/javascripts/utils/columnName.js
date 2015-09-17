bba81dc0ca0156fb7fe9ca4d022b82441c7994a3
Edmund Wright <edmund.wright@gmail.com>
2015-09-17 11:59:57 -0400

    Semicolons

--- app/assets/javascripts/utils/columnName.js
+++ app/assets/javascripts/utils/columnName.js
@@ -1,7 +1,7 @@
 GoogleSheetsClone.columnName = function (columnIndex) {
   return String.fromCharCode("A".charCodeAt() + columnIndex);
-}
+};
 
 GoogleSheetsClone.columnIndex = function (columnName) {
   return columnName.charCodeAt() - "A".charCodeAt();
-}
+};
