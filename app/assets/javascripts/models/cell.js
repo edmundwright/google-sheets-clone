56c25cf747a16255a6115900ae607b4ab9c14ca9
Edmund Wright <edmund.wright@gmail.com>
2015-10-13 13:02:34 -0400

    Revert unnecessary escaping of numeric attributes.

--- app/assets/javascripts/models/cell.js
+++ app/assets/javascripts/models/cell.js
@@ -11,9 +11,9 @@ GoogleSheetsClone.Models.Cell = Backbone.Model.extend ({
     if (this.get("contents_str") !== null) {
       return this.escape("contents_str");
     } else if (this.get("contents_int") !== null) {
-      return this.escape("contents_int");
+      return this.get("contents_int");
     } else if (this.get("contents_flo") !== null) {
-      return this.escape("contents_flo");
+      return this.get("contents_flo");
     }
   },
 
